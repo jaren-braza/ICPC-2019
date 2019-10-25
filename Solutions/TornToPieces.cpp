9245fbd009938062fec27552f25c6ac583d26b29
Jaren Braza <brazaj1@unlv.nevada.edu>
2019-10-25 13:32:33 -0700

    Replaced tabs with spaces for TornToPieces

--- Solutions/TornToPieces.cpp
+++ Solutions/TornToPieces.cpp
@@ -12,87 +12,87 @@ using namespace std;
 vector<string> getSplitParsedString();
 
 int main() {
-	int mapPieces;
-	cin >> mapPieces;
+    int mapPieces;
+    cin >> mapPieces;
 
-	// Skip over any hanging escape characters, like '\n'
-	cin.ignore();
+    // Skip over any hanging escape characters, like '\n'
+    cin.ignore();
 
-	// Track the unique neighbors for each station
-	map<string, set<string>> mapInfo;
+    // Track the unique neighbors for each station
+    map<string, set<string>> mapInfo;
 
-	// Parse each map piece
-	for (int i = 0; i < mapPieces; i++) {
-		vector<string> stationNeighbors = getSplitParsedString();
-		string describedStation = stationNeighbors[0];
+    // Parse each map piece
+    for (int i = 0; i < mapPieces; i++) {
+        vector<string> stationNeighbors = getSplitParsedString();
+        string describedStation = stationNeighbors[0];
 
-		// Store neighbors in the map info
-		// Note that relations are bi-directional
-		for (int j = 1; j < stationNeighbors.size(); j++) {
-			mapInfo[describedStation].insert(stationNeighbors[j]);
-			mapInfo[stationNeighbors[j]].insert(describedStation);
-		}
-	}
+        // Store neighbors in the map info
+        // Note that relations are bi-directional
+        for (int j = 1; j < stationNeighbors.size(); j++) {
+            mapInfo[describedStation].insert(stationNeighbors[j]);
+            mapInfo[stationNeighbors[j]].insert(describedStation);
+        }
+    }
 
-	// Parse the starting station and goal station
-	string start;
-	string goal;
-	cin >> start >> goal;
+    // Parse the starting station and goal station
+    string start;
+    string goal;
+    cin >> start >> goal;
 
-	// Track the parent station as { parent : source }
-	map<string, string> parent;
+    // Track the parent station as { parent : source }
+    map<string, string> parent;
 
-	// Perform a full graph search
-	queue<string> stationsToSearch;
-	stationsToSearch.push(start);
+    // Perform a full graph search
+    queue<string> stationsToSearch;
+    stationsToSearch.push(start);
 
-	while (!stationsToSearch.empty()) {
-		string current = stationsToSearch.front();
-		stationsToSearch.pop();
+    while (!stationsToSearch.empty()) {
+        string current = stationsToSearch.front();
+        stationsToSearch.pop();
 
-		// Search unvisited neighboring stations
-		// A station is marked as visited if it has a parent
-		for (string neighbor : mapInfo[current]) {
-			if (parent.find(neighbor) == parent.end()) {
-				parent[neighbor] = current;
-				stationsToSearch.push(neighbor);
-			}
-		}
-	}
+        // Search unvisited neighboring stations
+        // A station is marked as visited if it has a parent
+        for (string neighbor : mapInfo[current]) {
+            if (parent.find(neighbor) == parent.end()) {
+                parent[neighbor] = current;
+                stationsToSearch.push(neighbor);
+            }
+        }
+    }
 
-	// Print the path from the start to the goal station, if it exists
-	if (parent.find(goal) != parent.end()) {
-		vector<string> path;
-		string current = goal;
+    // Print the path from the start to the goal station, if it exists
+    if (parent.find(goal) != parent.end()) {
+        vector<string> path;
+        string current = goal;
 
-		while (current != start) {
-			path.push_back(current);
-			current = parent[current];
-		}
+        while (current != start) {
+            path.push_back(current);
+            current = parent[current];
+        }
 
-		path.push_back(start);
-		reverse(path.begin(), path.end());
+        path.push_back(start);
+        reverse(path.begin(), path.end());
 
-		for (string station : path)
-			cout << station << " ";
-		cout << endl;
-	}
-	else
-		cout << "no route found" << endl;
+        for (string station : path)
+            cout << station << " ";
+        cout << endl;
+    }
+    else
+        cout << "no route found" << endl;
 
-	return 0;
+    return 0;
 }
 
 vector<string> getSplitParsedString() {
-	vector<string> splitString;
-	string str;
-	string substring;
+    vector<string> splitString;
+    string str;
+    string substring;
 
-	getline(cin, str);
-	istringstream stream(str);
+    getline(cin, str);
+    istringstream stream(str);
 
-	while (stream >> substring)
-		splitString.push_back(substring);
+    while (stream >> substring)
+        splitString.push_back(substring);
 
-	return splitString;
+    return splitString;
 }
